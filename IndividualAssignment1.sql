477f90f663f4a67f9dd3751eb4fc00a96d20fbd5
Josh Peyton <joshpeyton@hotmail.com>
2016-10-18 16:29:16 +0100

    Database Individual Assignment
    This is an individual assignment from year two in college.  The assignment included creating queries, sub-queries, transactions etc from a table we had to populate for a group assignment

---
+++ IndividualAssignment1.sql
@@ -0,0 +1,210 @@
+drop table matches;
+/*Insert into table Teams*/
+Insert into Team(member1) values(100);
+Insert into Team(member1) values(101);
+Insert into Team(member1) values(102);
+Insert into Team(member1) values(103);
+Insert into Team(member1) values(104);
+Insert into Team(member1) values(105);
+Insert into Team(member1) values(106);
+Insert into Team(member1) values(107);
+Insert into Team(member1) values(108);
+Insert into Team(member1) values(109);
+Insert into Team(member1) values(110);
+Insert into Team(member1) values(111);
+Insert into Team(member1) values(112);
+Insert into Team(member1) values(113);
+Insert into Team(member1) values(114);
+Insert into Team(member1) values(115);
+Insert into Team(member1) values(116);
+/*Insert to table Matches*/ 
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(1, '8-Jan-16', '30-Mar-16', 6.2,  1.3,  6.2);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(2, '7-Jan-16', '30-Mar-16', 4.2,  3.5, 2.3);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(3, '18-Jan-16', '30-Mar-16', 5.3,  1.5,  1.6);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(4, '22-Jan-16', '30-Mar-16', 3.1,  4.6,  3.5);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(5, '29-Jan-16', '30-Mar-16', 5.5,  1.3,  1.3);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(6, '18-Jan-16', '30-Mar-16', 2.7,  5.3,  3.6);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(7, '23-Feb-16', '30-Mar-16', 1.2,  1.6,  2.5);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(8, '4-Feb-16', '30-Mar-16', 3.4,  4.3,  3.6);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(9, '8-Feb-16', '30-Mar-16', 1.3,  8.5,  8.5);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(10, '3-Feb-16', '30-Mar-16', 2.6,  2.3,  4.6);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(11, '14-Feb-16', '30-Mar-16', 2.5,  6.7,  2.5);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(12, '9-Mar-16', '30-Mar-16', 6.4,  1.2,  4.9);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(13, '3-Mar-16', '30-Mar-16', 3.5,  3.6,  0.0);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(14, '7-Mar-16', '30-Mar-16', 2.2,  8.4,  4.7);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(15, '25-Mar-16', '30-Mar-16', 3.4,  7.5,  5.7);
+Insert into matches(MatchNum,PlayDate,PlayByDate, set1score,set2score,set3score  ) values(16, '1-Mar-16', '30-Mar-16', 3.4,  7.5,  2.3);
+
+/*Insert into table memberinfo*/
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(100, 'Mary Black', 'Private', 11, '10-Oct-11', '1-Jan-12');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(101, 'Margaret Star', 'Social', '5-Jul-11', '1-Jan-13');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(103, 'Harry Kane', 'Private',  32, '1-Feb-12', '1-Jan-14');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(104, 'Molly Malone', 'Social',  '1-Jan-12', '1-Jan-15');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(105, 'Joan Burton', 'Private',  68, '1-DEC-12', '1-Jan-15');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(106, 'Jessie Jay', 'Social', '1-Nov-12', '1-Jan-16');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(107, 'Britney Spears', 'Private',  82, '1-FEb-12', '1-Jan-16');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(108, 'Aideen O Brien', 'Social', '1-Jan-12', '1-Jan-13');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(109, 'Chloe Peyton', 'Private',  48, '1-Apr-12', '1-Jan-13');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(110, 'Michelle Collins', 'Social', '1-Mar-12', '1-Jan-14');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(111, 'Michelle Obama', 'Private',  28, '1-May-12', '1-Jan-15');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(112, 'Mary Harney', 'Social', '1-Jun-12', '1-Jan-15');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(113, 'Francis Murtagh', 'Private',  38, '1-July-12', '1-Jan-15');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(114, 'Jess Gylne', 'Private',  18, '1-Aug-12', '1-Jan-16');
+Insert into  memberinfo(memberid,memname,memtype1,seeding,membershipstart,membershipend) values(115, 'Ri Ri', 'Private',  80, '1-Dec-12', '1-Jan-13');
+Insert into  memberinfo(memberid,memname,memtype1,membershipstart,membershipend) values(116, 'Queen Beyonce', 'Social', '1-Jan-12', '1-Jan-15' );
+
+/*Projection and Selection
+Selects Match Num from table matches where set1score is 
+greater than set2score*/
+SELECT MatchNum FROM matches where set1score < set2score;
+
+SELECT MatchNum
+FROM matches
+WHERE MatchNum > 5;
+
+/*Sort Matches by the Date the match is played 
+or by the date the match must be played by */
+SELECT * FROM matches
+ORDER BY PlayDate;
+
+/*Non aggregate function
+MatchNum is selected from matches
+PlayDate is equal to 18-Jan-16*/
+SELECT MatchNum FROM matches WHERE PlayDate='18-Jan-16';
+
+/*Testing for nulls
+This query tests the set scores
+to check if any are equal to null*/
+SELECT MatchNum 
+from matches 
+where set1score ||set2score ||set3score  is null;
+
+/*Aggregation Group By
+select MatchNum from matches
+where set1score is greater 
+than set3score and group by 
+playdate*/
+SELECT MatchNum
+FROM matches
+WHERE set1score>set3score
+GROUP BY PlayDate;
+
+
+/*Aggregation Having Clause needs fixing
+select MatchNum, PlayDate, PlayByDate, set1score, 
+set2score, set3score group by set1score is greater 
+than 3*/
+SELECT MatchNum, PlayDate, PlayByDate, set1score, set2score, set3score
+FROM matches
+GROUP BY set1score
+HAVING COUNT(set1score) > 3;
+
+/*Minus
+This query minus' the first query 
+from the second so matchnum of set1score>set3score
+minus matchnum of set2score>set3score
+*/
+SELECT MatchNum
+FROM matches
+WHERE set1score>set3score
+MINUS
+SELECT MatchNum
+FROM matches
+WHERE set2score>set3score;
+
+/*Intersect
+Common data in these queries are 
+intersected. The match nums of
+set2score < set3score and 
+set1score < set3score*/
+SELECT MatchNum
+FROM matches
+WHERE set2score < set3score
+
+INTERSECT
+
+SELECT MatchNum
+FROM matches
+WHERE set1score < set3score;
+
+
+/*Union
+match num table matches joins memberid from table memberinfo*/
+SELECT  MatchNum FROM matches
+UNION
+SELECT memberid FROM memberinfo;
+
+SELECT  PlayDate FROM matches
+UNION
+SELECT PlayByDate FROM matches;
+
+/*Full Outer Join*/
+SELECT matches.MatchNum, memberinfo.memberid
+FROM matches
+FULL OUTER JOIN memberinfo
+ON matches.set1score<matches.set1score
+ORDER BY matches.PlayDate;
+
+/*Inner Join
+select from member info on memberid = member1*/
+SELECT memberid, memname,memtype1,seeding,membershipstart
+     FROM memberinfo
+     INNER JOIN Team
+     ON memberinfo.memberid = Team.member1;
+
+/*Sub Query
+select setscores from matches where mathnum less
+than 5 */
+SELECT set1score,set2score, set3score
+FROM   matches
+WHERE  PlayDate IN
+      (SELECT PlayDate
+      FROM matches
+      WHERE MatchNum < 5);
+      
+/*Query
+This quet displays the matchnum, playdate
+and playbydate depending on the setscores values.
+This query checks whichs scores are greater than 
+or less than than others*/
+SELECT MatchNum, PlayDate, PlayByDate
+FROM matches
+WHERE 
+set1score>set2score 
+AND 
+set1score>set3score
+OR 
+set1score>set2score
+AND 
+set1score<set3score
+OR
+set2score>set3score 
+AND 
+set2score>set1score
+OR 
+set2score>set3score
+AND 
+set2score<set1score
+ORDER BY PlayDate;
+
+/*Transaction
+This transaction uses Savpoints so I 
+can decide where I want to rollback 
+to in the Transaction. This transaction deletes rows 
+where set1score is greater than set2score, 
+where set2score is greater than set3score and 
+where set1score is greater than set3score then rolls
+back to savepoint 1 therefore nothing was deleted*/
+SAVEPOINT SP1;
+DELETE FROM matches 
+WHERE set1score<set2score;
+SAVEPOINT SP2;
+DELETE FROM matches 
+WHERE set2score<set3score;
+SAVEPOINT SP3;
+DELETE FROM matches 
+WHERE set1score<set3score;
+
+ROLLBACK TO SP1;
+
+
